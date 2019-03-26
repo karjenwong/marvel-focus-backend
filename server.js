51e7ead7287b6c2cbb579db5a8349a42332f00d3
Karjen Wong <karjenwong@gmail.com>
2019-03-25 18:35:21 -0700

    add test route

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-const express = require('express')
-const app = express()
-const PORT = 8080
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const marvel = require('./routes/marvel')
-const movie = require('./routes/movie')
-const youtube = require('./routes/youtube')
-app.use(bodyParser.json())
-app.use(cors())
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const marvel = require("./routes/marvel");
+const movie = require("./routes/movie");
+const youtube = require("./routes/youtube");
+const test = require("./routes/test")
+app.use(bodyParser.json());
+app.use(cors());
 
-
-app.use('/marvel', marvel)
-app.use('/movie', movie)
-app.use('/youtube', youtube)
+app.use(express.static(__dirname + "./../build"));
+app.use("/marvel", marvel);
+app.use("/movie", movie);
+app.use("/youtube", youtube);
+app.use("test", test);
 
 app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`)
-})
+  console.log(`listening on port ${PORT}`);
+});
